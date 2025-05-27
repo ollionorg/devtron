41a3f07294b28e51e5111eef230e561c8a7f4347
ankur-ollion <149468003+ankur-ollion@users.noreply.github.com>
2025-05-27 17:18:29 +0530

    Command in new lines

--- scripts/staging.sh
+++ scripts/staging.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
-apt-get update -y && apt-get install -y unzip curl wget && curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && unzip awscliv2.zip && ls -al && aws/install && aws --version && export AWS_ACCESS_KEY_ID=$aws_access_key_id && export AWS_SECRET_ACCESS_KEY=$aws_secret_access_key && ls -al sourcecode && wget https://github.com/ekristen/aws-nuke/releases/download/v3.52.2/aws-nuke-v3.52.2-linux-amd64.tar.gz && tar -xvf aws-nuke-v3.52.2-linux-amd64.tar.gz && chmod +x aws-nuke && mv aws-nuke /usr/local/bin/aws-nuke && aws-nuke nuke -c sourcecode/config/global.yaml --force | | grep "would remove" && echo complete
+apt-get update -y
+apt-get install -y unzip curl wget
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip && ls -al && aws/install 
+aws --version 
+export AWS_ACCESS_KEY_ID=$aws_access_key_id 
+export AWS_SECRET_ACCESS_KEY=$aws_secret_access_key
+ls -al sourcecode
+wget https://github.com/ekristen/aws-nuke/releases/download/v3.52.2/aws-nuke-v3.52.2-linux-amd64.tar.gz 
+tar -xvf aws-nuke-v3.52.2-linux-amd64.tar.gz && chmod +x aws-nuke
+mv aws-nuke /usr/local/bin/aws-nuke && aws-nuke nuke -c sourcecode/config/global.yaml --force | grep "would remove" 
+echo complete
