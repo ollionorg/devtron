2f16819b3d1da24a20d06e80753df8d9dc22632e
ankur-ollion <149468003+ankur-ollion@users.noreply.github.com>
2025-06-12 19:44:18 +0530

    Update aws-nuke.sh

--- scripts/aws-nuke.sh
+++ scripts/aws-nuke.sh
@@ -27,12 +27,27 @@ fi
 
 apt-get update -y
 apt-get install -y unzip curl wget
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+
+# Get the machine architecture
+ARCH=$(uname -m)
+# Check if the architecture is aarch64
+if [ "$ARCH" = "x86_64" ]; then
+    echo $(uname -m)
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+elif [ "$ARCH" = "aarch64" ]; then
+    echo $(uname -m)
+    curl 'https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip' -o "awscliv2.zip"   
+else
+    echo "Machine architecture not matched"
+    exit 1
+fi
+
 unzip awscliv2.zip && ls -al && aws/install 
 aws --version 
 export AWS_ACCESS_KEY_ID=$aws_access_key_id 
 export AWS_SECRET_ACCESS_KEY=$aws_secret_access_key
 ls -al sourcecode
+echo "Started installing aws-nuke"
 wget https://github.com/ekristen/aws-nuke/releases/download/v3.52.2/aws-nuke-v3.52.2-linux-amd64.tar.gz 
 tar -xvf aws-nuke-v3.52.2-linux-amd64.tar.gz
 chmod +x aws-nuke
